a242ce18b42ac6e90a0cfed1381a54cf4a7d9266
Victor Olave <victorolave1131@gmail.com>
2019-08-12 01:32:20 -0500

    File Creation

--- src/Logger/FileUtil.java
+++ src/Logger/FileUtil.java
@@ -1,8 +1,21 @@
 package Logger;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileUtil {
 
-    public void writeToFile(String file, String msg, boolean a, boolean b) {
-        System.out.println("The message: " + msg + ", is saved in " + file);
+    public void writeToFile(String fileName, String msg, boolean a, boolean b) {
+        try
+        {
+            FileWriter fileWriter = new FileWriter("Your Path/"+fileName);
+            fileWriter.write(msg);
+            fileWriter.close();
+            System.out.println("The message: " + msg + ", saved succesfully in " + fileName);
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
     }
 }
